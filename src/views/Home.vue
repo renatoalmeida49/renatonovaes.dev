f2e0b736ac5acc8085340d60007532dda859fdb8
Renato Novaes <renatoalmeida49@gmail.com>
2021-09-26 17:02:50 -0300

    feat(home.vue): content removed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -22,69 +22,13 @@
     </div>
   </section>
 
-  <section id="sobre">
-    <Title>Sobre</Title>
-
-    <p>Estou na área de TI desde 2011. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, alias quis tempora laborum id maxime rem voluptatem sit praesentium vel porro eum quaerat, nobis ipsam neque facere nisi animi nulla.</p>
-
-    <img src="@/assets/img/me_2.jpg" />
-
-    <h2>Desenvolvedor Frontend</h2>
-
-    <ul>
-      <li><i class="fas fa-caret-right fa-lg"></i><span>Nascimento:</span> 25 Oct 1996</li>
-      <li><i class="fas fa-caret-right fa-lg"></i><span>Website:</span> Esse mesmo</li>
-      <li><i class="fas fa-caret-right fa-lg"></i><span>Phone:</span> +55 82 9 9690 1034</li>
-      <li><i class="fas fa-caret-right fa-lg"></i><span>Cidade:</span> Maceió-AL, Brasil</li>
-      <li><i class="fas fa-caret-right fa-lg"></i><span>Educação:</span> Superior</li>
-      <li><i class="fas fa-caret-right fa-lg"></i><span>Freelance:</span> Disponível</li>
-    </ul>
-
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic minus velit dolore, asperiores sint aspernatur officiis ipsam est labore corporis dolorem, iure assumenda ratione. Quia nesciunt nam tempora provident quae.</p>
-  </section>
-
-  <section id="fatos">
-    <Title>Fatos</Title>
-
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non iure nostrum aut magnam recusandae commodi deleniti! Distinctio temporibus nobis animi adipisci explicabo, tempore facilis quae aliquam! Sit eaque nemo asperiores.</p>
-
-    <div class="card">
-      <i class="fas fa-smile-beam fa-6x"></i>
-      <span>232</span>
-      <p>Clientes felizes</p>
-    </div>
-
-    <div class="card">
-      <i class="fas fa-project-diagram fa-6x"></i>
-      <span>232</span>
-      <p>Projetos</p>
-    </div>
-
-    <div class="card">
-      <i class="fas fa-headset fa-6x"></i>
-      <span>232</span>
-      <p>Horas de suporte</p>
-    </div>
-
-    <div class="card">
-      <i class="fas fa-users fa-6x"></i>
-      <span>232</span>
-      <p>Colegas</p>
-    </div>
-  </section>
-
-  <section id="habilidades"></section>
 </div>
 </template>
 
 <script>
-import Title from "@/components/Title.vue"
-
 export default {
   name: 'Home',
-  components: {
-    Title
-  },
+
   data() {
     return {
       showMenu: false,
