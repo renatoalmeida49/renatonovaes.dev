522f0fafef6023e2ab302fa59a55c45526182230
Renato Novaes <renatoalmeida49@gmail.com>
2021-09-27 09:26:41 -0300

    feat(hello.vue): social media

--- src/views/components/hello.vue
+++ src/views/components/hello.vue
@@ -3,6 +3,14 @@
   <section class="hello d-flex flex-column justify-content-center">
     <div class="hello__container">
       <h1 class="hello__title">Renato Novaes</h1>
+
+      <div class="hello__social">
+        <template v-for="(social, index) in socialLinks">
+          <a :key="index" :href="social.link" target="blank">
+            <box-icon :name="social.icon" type="logo" color="#45505b" />
+          </a>
+        </template>
+      </div>
     </div>
   </section>
 
@@ -12,7 +20,34 @@
 <script>
 
 export default {
-  name: "Hello"
+  name: "Hello",
+
+  data() {
+    return {
+      socialLinks: [
+        {
+          link: "https://www.facebook.com/renatonovaes49",
+          icon: "facebook",
+          label: "Facebook"
+        },
+        {
+          link: "https://github.com/renatoalmeida49",
+          icon: "github",
+          label: "Github"
+        },
+        {
+          link: "https://www.linkedin.com/in/renatonovaes49",
+          icon: "linkedin",
+          label: "Linkedin"
+        },
+        {
+          link: "https://www.instagram.com/novaes_r",
+          icon: "instagram",
+          label: "Instagram"
+        },
+      ]
+    }
+  }
 }
 
 </script>
@@ -49,12 +84,25 @@ export default {
     line-height: 56px;
     color: #45505b;
   }
+
+  &__social {
+    padding: 20px 0;
+
+    a {
+      margin-right: 20px;
+    }
+  }
 }
 
 @media (max-width: 992px) {
   .hello {
     text-align: center;
     padding-left: 0;
+
+    &__title {
+      font-size: 32px;
+      line-height: 36px;
+    }
   }
 }
 </style>
